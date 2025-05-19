9901ea88bc25a24547070da32ea900da75fbe20a
22130030 <22130030@st.hcmuaf.edu.vn>
2025-05-20 06:17:08 +0700

    Load data

--- backend/user_service/src/main/java/vn/hcmuaf/edu/vn/user_service/dto/CustomerDTO.java
+++ backend/user_service/src/main/java/vn/hcmuaf/edu/vn/user_service/dto/CustomerDTO.java
@@ -8,8 +8,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class CustomerDTO {
     private Long id;
 
@@ -24,4 +24,55 @@ public class CustomerDTO {
     private String email;
 
     private String address;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(Long id, String fullName, String phoneNumber, String email, String address) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Họ tên không được để trống") String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(@NotBlank(message = "Họ tên không được để trống") String fullName) {
+        this.fullName = fullName;
+    }
+
+    public @NotBlank(message = "Số điện thoại không được để trống") @Pattern(regexp = "^\\d{10,11}$", message = "Số điện thoại không hợp lệ") String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(@NotBlank(message = "Số điện thoại không được để trống") @Pattern(regexp = "^\\d{10,11}$", message = "Số điện thoại không hợp lệ") String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public @Email(message = "Email không hợp lệ") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Email(message = "Email không hợp lệ") String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- backend/user_service/src/main/java/vn/hcmuaf/edu/vn/user_service/repository/CustomerRepository.java
+++ backend/user_service/src/main/java/vn/hcmuaf/edu/vn/user_service/repository/CustomerRepository.java
@@ -8,8 +8,16 @@ import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    // 7.1.4 existsByPhoneNumber(phoneNumber) (Kiểm tra số điện thoại trong DB)
+    // 7.1.5 SELECT COUNT(*) > 0 FROM customers WHERE phone_number = ? (Truy vấn SQL)
     boolean existsByPhoneNumber(String phoneNumber);
+
+    // 7.1.10 existsByEmail(email) (Kiểm tra email)
+    // 7.1.11 SELECT COUNT(*) > 0 FROM customers WHERE email = ? (Truy vấn SQL)
     boolean existsByEmail(String email);
+
     Optional<Customer> findByPhoneNumber(String phoneNumber);
+
     Optional<Customer> findByEmail(String email);
 }
